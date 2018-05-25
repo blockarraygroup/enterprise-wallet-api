04ee16f51d2126cd5a514711d7a59f78c5dc9030
asifblockarray <khan@blockarray.com>
2018-05-25 11:51:34 +0500

    contract fillable fields updated

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -6,7 +6,7 @@ use Jenssegers\Mongodb\Eloquent\Model;
 class Contract extends Model {
 	protected $collection = 'contracts';
 
-	protected $fillable = ['name', 'address', 'tx_hash', 'user_id'];
+	protected $fillable = ['name', 'address', 'tx_hash', 'user_id', 'deploy_type', 'abi'];
 	protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 
 	public function user () {
